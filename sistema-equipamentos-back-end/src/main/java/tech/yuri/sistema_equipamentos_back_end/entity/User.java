0e843a86717223df51201e49998a9c06c2a82518
YuriGabrielR <yurigabriel21@yahoo.com>
2025-03-13 10:02:39 -0300

    feat: criação da entidade User

---
+++ sistema-equipamentos-back-end/src/main/java/tech/yuri/sistema_equipamentos_back_end/entity/User.java
@@ -0,0 +1,75 @@
+package tech.yuri.sistema_equipamentos_back_end.entity; 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="tb_usuarios")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private String id; 
+
+    private String nome; 
+
+
+    private String email; 
+
+    private String password; 
+
+
+    @Enumerated(EnumType.STRING)
+    private Role role; 
+
+
+    public enum Role { ADMIN, USER, TECNICO; }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    
+}
