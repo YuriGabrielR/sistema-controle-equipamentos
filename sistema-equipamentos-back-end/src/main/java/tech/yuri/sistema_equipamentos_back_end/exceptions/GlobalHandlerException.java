9f101505bcecc2d56d52ffd88a407b9adb11dd91
YuriGabrielR <yurigabriel21@yahoo.com>
2025-03-14 16:24:43 -0300

    feat: adição de novo exception handler, mais especificamente relacionado ao valid

--- sistema-equipamentos-back-end/src/main/java/tech/yuri/sistema_equipamentos_back_end/exceptions/GlobalHandlerException.java
+++ sistema-equipamentos-back-end/src/main/java/tech/yuri/sistema_equipamentos_back_end/exceptions/GlobalHandlerException.java
@@ -1,10 +1,13 @@
 package tech.yuri.sistema_equipamentos_back_end.exceptions; 
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,7 +23,7 @@ public ResponseEntity<Object> handleUsuarioNaoEncontradoException(UsuarioNaoEnco
   
     response.put("error", ex.getMessage());
     response.put("Message", "usuário não encontrado");
-    
+
     return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
 }
 
@@ -34,6 +37,25 @@ public ResponseEntity<Object> handleEmailExisteException(EmailExistenteException
     response.put("Message", ex.getMessage());
 
     return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+}
+
+
+@ExceptionHandler(MethodArgumentNotValidException.class)
+public ResponseEntity<Object> handleValidationsContraints (MethodArgumentNotValidException ex){
+
+    Map<String, Object> response = new HashMap<>();     
+
+     List<String> errorMessages = ex.getBindingResult()
+                                    .getAllErrors()
+                                    .stream()
+                                    .map(error -> error.getDefaultMessage())
+                                    .collect(Collectors.toList());
+
+    response.put("Message", errorMessages);
+    response.put("Error", "Erro de validação");
+
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST); 
+
 }
 
 }
